2c6e4b5071df1045b51f93ca9f45aa5c8422bb4e
James Dean <deany3791@gmail.com>
2022-01-20 23:03:26 +0000

    Uncommented unused functions so tests pass.

--- src/banana.py
+++ src/banana.py
@@ -8,46 +8,47 @@ import requests
 import urllib
 
 # NOTE holding onto this function. Could be of use in the future.
-# def get_last_weekday_date(date_: dt.date, day_name: str = "monday") -> dt.date:
-#     """Given a date and day of the week, return datetime of the most recent occurance of said day of week before given date.
-#     Will return date_ -7 days if day of week is on that date.
+def get_last_weekday_date(date_: dt.date, day_name: str = "monday") -> dt.date:
+    """Given a date and day of the week, return datetime of the most recent occurance of said day of week before given date.
+    Will return date_ -7 days if day of week is on that date.
 
-#     Args:
-#         date_ (dt.date): Any date.
-#         day_name (str, optional): One of seven days of the week eg 'monday'. Defaults to "monday".
+    Args:
+        date_ (dt.date): Any date.
+        day_name (str, optional): One of seven days of the week eg 'monday'. Defaults to "monday".
 
-#     Returns:
-#         dt.date: Most recent occurance of said day of week before given date.
-#     """
-#     days_of_week = [
-#         "sunday",
-#         "monday",
-#         "tuesday",
-#         "wednesday",
-#         "thursday",
-#         "friday",
-#         "saturday",
-#     ]
-#     target_day = days_of_week.index(day_name.lower())
-#     delta_day = target_day - date_.isoweekday()
-#     if delta_day >= 0:
-#         delta_day -= 7  # go back 7 days
-#     return date_ + dt.timedelta(days=delta_day)
+    Returns:
+        dt.date: Most recent occurance of said day of week before given date.
+    """
+    days_of_week = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+    ]
+    target_day = days_of_week.index(day_name.lower())
+    delta_day = target_day - date_.isoweekday()
+    if delta_day >= 0:
+        delta_day -= 7  # go back 7 days
+    return date_ + dt.timedelta(days=delta_day)
 
-# def get_file_name(date_: dt.date) -> str:
-#     """Transform date into formatted government banana file string.
-#     Args:
-#         date_ (dt.date): Any date.
-#     Returns:
-#         str: Formatted government banana file string.
-#     """
 
-#     # NOTE there is not a clean way to do this with a single strftime due to the nature of the gov's file name format.
-#     day_of_month = date_.day
-#     month_ = date_.strftime("%b").lower()
-#     year_ = date_.strftime("%y")
+def get_file_name(date_: dt.date) -> str:
+    """Transform date into formatted government banana file string.
+    Args:
+        date_ (dt.date): Any date.
+    Returns:
+        str: Formatted government banana file string.
+    """
 
-#     return f"bananas-{day_of_month}{month_}{year_}.csv"
+    # NOTE there is not a clean way to do this with a single strftime due to the nature of the gov's file name format.
+    day_of_month = date_.day
+    month_ = date_.strftime("%b").lower()
+    year_ = date_.strftime("%y")
+
+    return f"bananas-{day_of_month}{month_}{year_}.csv"
 
 
 def get_file_url() -> str:
